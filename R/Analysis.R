a787d8b05c9400d6b4ab628b0b98b8bee5d6b8ae
RaikOtto <fubioinf@gmail.com>
2015-11-20 16:13:20 +0100

    update

--- R/Analysis.R
+++
@@ -1,6 +0,0 @@
-#' Start the identification of a cancer cell lines based on a vcf file
-#' @export
-identify_cancer_cell_line = function( vcf_file_path  ){
-  
-  
-}
--- R/Identify_VCF_file.R
+++ R/Identify_VCF_file.R
@@ -1,6 +1,6 @@
 #' Parses the vcf file and predicts the identity of the sample
 #' @export
-identify_vcf_file = function( vcf_file_path ){
+identify_vcf_file = function( vcf_file_path, output_path = "" ){
   
   print( paste0( "Creating fingerprint from VCF file ", vcf_file_path  ) )
   vcf_fingerprint = parse_vcf_file( vcf_file_path )
@@ -10,70 +10,46 @@ identify_vcf_file = function( vcf_file_path ){
   
   attach( sim_list_file  )
   
-  print( "Finished loading similarity data"  )
+  print( "Finished loading similarity data. Mapping vcf's fingerprint to all contained fingerprints"  )
   
-  #init_connection = function ( db_path ){
-
-   # if ( grepl( "Younikorn.db", c(db_path)) != T )
-    #  db_path = paste( db_path, "inst/Younikorn.db", sep = "/" )
-
-  #  drv = dbDriver("SQLite")
-  #  con = dbConnect(
-   #   drv,
-  #    dbname = db_path
-  #  )
-  #  con = src_sqlite( con@dbname )
-      
-  #  return( con )
-  #}
+  path_to_output_db = paste( system.file("", package="Younikorn"), "parsed_DB.tab", sep ="/") 
+  path_to_output_dict = paste( system.file("", package="Younikorn"), "parsed_dict.tab", sep ="/")
+  cl_data          = read.table( path_to_output_db,   sep ="\t", header = T )
+  all_fingerprints = read.table( path_to_output_dict, sep ="\t", colClasses = c( "character", 'NULL'), header =T )$Fingerprint
   
-  #full_con = init_connection( db_path )
-  
-  #raw_res = as.data.frame(
-
-  #   dplyr::filter( 
-  #    tbl( full_con, from = "similarity_matrix" ),
-  #    mutational_similarity_marker %in% vcf_fingerprint
- #    )
- # )
- # cl_names = colnames(raw_res)[-1]
-
- # res_common = as.matrix(
- #   raw_res[ , -1]
- # )
- # res_common = matrix( as.integer(res_common), ncol = dim(res_common)[2]  )
-  
-  ###
-  
-  #res_common = matrix( as.integer( unlist( sim_list ) ), ncol = length( sim_list)  )
-  #mapping    = match( rownames(res_common), vcf_fingerprint, nomatch = 0 )
-  #mapping[ mapping != 0 ] = 1
+  mapping = match( all_fingerprints, vcf_fingerprint, nomatch = 0 )
   
   adv = 0
   nr_cls = length( sim_list)
   
   match_fp = function( sim_list_entry ){
     
-    stat = round( (adv / as.double(nr_cls)) * 100, 1 )
+    stat = round( (adv / as.double(nr_cls)) * 100, 0 )
     adv <<- adv + 1
     
-    if ( stat != round( (adv / as.double(nr_cls)) * 100, 1 ) )
-      print( paste( round( (adv / as.double(nr_cls)) * 100, 1 ), "% finished", sep =" " ) )
+    if ( stat != round( (adv / as.double(nr_cls)) * 100, 0 ) )
+      print( paste( round( (adv / as.double(nr_cls)) * 100, 0 ), "%", sep =" " ) )
     
     return( sum( as.integer(mapping) & as.integer( sim_list_entry ) ) )
   }
   
   hits = unlist( lapply( sim_list, FUN = match_fp ) )
-  hits = unlist(identification)
-  
-  match_index     = hits != 0
-  candidates      = cl_data$CL[ order(hits, decreasing = T)  ]
+  candidates = cl_data$CL[ order(hits, decreasing = T)  ]
 
   print( paste0("Best candidate: ", candidates[1] )  )
-
-  #res_common_filt = res_common[ match_index ]
-  #table( res_common_filt)
   
-  #identify_vcf_fingerprint( vcf_fingerprint  )
+  res_tab = data.frame(
     
+    "Amount_hits" = hits[order(hits, decreasing = T)],
+    "CL_identifier" = candidates
+  )
+  
+  library("stringr")
+  
+  res_tab[1:5,]
+  
+  if ( output_path == "" )
+    output_path = paste0( vcf_file_path, ".identification.tab" )
+  
+  write.table( file = output_path, res_tab, sep ="\t", row.names = F, quote = F  )
 }
