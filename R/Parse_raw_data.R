1b90617bce6810d8bca113454d3817b31cf50b61
RaikOtto <fubioinf@gmail.com>
2016-01-13 17:03:29 +0100

    update

--- R/Parse_raw_data.R
+++ R/Parse_raw_data.R
@@ -10,28 +10,33 @@ initiate_canonical_databases = function(
     ccle_file = "CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.maf",
     ref_gen = "GRCH37",
     distinct_mode = TRUE
-  ){
+    ){
   
-  require( "DBI",     quietly = TRUE, warn.conflicts = FALSE)
-  require( "RSQLite", quietly = TRUE, warn.conflicts = FALSE)
-  require( "stringr", quietly = TRUE, warn.conflicts = FALSE)
+    require( "DBI",     quietly = TRUE, warn.conflicts = FALSE)
+    require( "RSQLite", quietly = TRUE, warn.conflicts = FALSE)
+    require( "stringr", quietly = TRUE, warn.conflicts = FALSE)
+    
+    print( c( "Reference genome: ", ref_gen )  )
+    
+    ### pre processing
+    
+    package_path    = system.file("", package="Uniquorn")
+    database_path =  paste( package_path, "uniquorn_distinct_panels_db.sqlite", sep ="/" )
+    
+    if (!distinct_mode)
+        database_path   =  paste( package_path, "uniquorn_non_distinct_panels_db.sqlite", sep ="/" )
+    
+    database_default_path =  paste( package_path, "uniquorn_db_default.sqlite", sep ="/" )
+    
+    drv = RSQLite::SQLite()
+    con = DBI::dbConnect(drv, dbname = database_default_path)
   
-  print( c( "Reference genome: ", ref_gen )  )
-  
-  ### pre processing
-  
-  package_path    = system.file("", package="Uniquorn")
-  database_path =  paste( package_path, "uniquorn_distinct_panels_db.sqlite", sep ="/" )
-  
-  if (!distinct_mode)
-    database_path   =  paste( package_path, "uniquorn_non_distinct_panels_db.sqlite3", sep ="/" )
-  
-  database_default_path =  paste( package_path, "uniquorn_db_default.sqlite3", sep ="/" )
-  sim_list = as.data.frame( tbl( src_sqlite( database_default_path ), "sim_list_df" ), n = -1 )
-  sim_list = sim_list[, which( colnames(sim_list) != "Ref_Gen"  ) ]
-  sim_list = sim_list[, which( colnames(sim_list) != "Weight"  ) ]
+    sim_list = as.data.frame( DBI::dbReadTable( con, "sim_list") )
+    dbDisconnect(con)
+    sim_list = sim_list[, which( colnames(sim_list) != "Ref_Gen"  ) ]
+    sim_list = sim_list[, which( colnames(sim_list) != "Weight"  ) ]
 
-  parse_files = c()
+    parse_files = c()
   
     if (file.exists(cosmic_file)){
       
@@ -47,87 +52,87 @@ initiate_canonical_databases = function(
         parse_files = c(parse_files, cosmic_file)
     }
   
-  if (file.exists(ccle_file)){
+    if (file.exists(ccle_file)){
       
       print( c( "Found CCLE: ", file.exists( ccle_file ) )  )
       sim_list = parse_ccle_genotype_data( ccle_file, sim_list )
       parse_files = c(parse_files, ccle_file)
-  }
-  
-  if (length(parse_files) == 0)
+    }
+    
+    if (length(parse_files) == 0)
       stop("Did not find CCLE & CoSMIC CLP file! Aborting.")
-  
-  # overwrite existing db
-  if (file.exists(database_path))
-    file.remove( database_path )
+    
+    # overwrite existing db
+    if (file.exists(database_path))
+        file.remove( database_path )
+    
+    print("Started pre-calculations")
+    
+    if ( exists("sim_list_stats"))
+        rm( sim_list_stats )
+    
+    print("Finished parsing, aggregating over parsed Cancer Cell Line data")
+    
+    list_of_cls = unique( sim_list$CL )
+    panels = sapply( list_of_cls, FUN = str_split, "_"  )
+    panels = as.character(unique( as.character( sapply( panels, FUN = tail, 1) ) ))
+    
+    if (!distinct_mode){
+        panels = paste0( c(panels), collapse ="|"  )
+        database_path =  paste( package_path, "uniquorn_non_distinct_panels_db.sqlite3", sep ="/" )
+    }
+    
+    print( paste( "Distinguishing between panels:",paste0( c(panels), collapse = ", "), sep = " ") )
 
-   print("Started pre-calculations")
-  
-  if ( exists("sim_list_stats"))
-    rm( sim_list_stats )
-  
-  print("Finished parsing, aggregating over parsed Cancer Cell Line data")
-  
-  list_of_cls = unique( sim_list$CL )
-  panels = sapply( list_of_cls, FUN = str_split, "_"  )
-  panels = as.character(unique( as.character( sapply( panels, FUN = tail, 1) ) ))
-  
-  if (!distinct_mode){
-    panels = paste0( c(panels), collapse ="|"  )
-    database_path =  paste( package_path, "uniquorn_non_distinct_panels_db.sqlite3", sep ="/" )
-  }
-  
-  print( paste( "Distinguishing between panels:",paste0( c(panels), collapse = ", "), sep = " ") )
-  
-  for (panel in panels) {
-  
-    print(panel)
-    
-    sim_list_panel   = sim_list[ grepl( panel, sim_list$CL) , ]
-    member_var_panel = rep( 1, dim(sim_list_panel)[1] )
-    
-    sim_list_stats_panel = aggregate( member_var_panel , by = list( sim_list_panel$CL ), FUN = sum )
-    colnames(sim_list_stats_panel) = c( "CL", "Count" )
-    
-    print("Aggregating over mutational frequency to obtain mutational weight")
+      for (panel in panels) {
+      
+        print(panel)
         
-    weights_panel = aggregate( member_var_panel , by = list( sim_list_panel$Fingerprint ), FUN = sum )
-    weights_panel$x = 1.0 / as.double( weights_panel$x )
-    
-    mapping_panel = match( as.character( sim_list_panel$Fingerprint ), as.character( weights_panel$Group.1) )
-    sim_list_panel = cbind( sim_list_panel, weights_panel$x[mapping_panel] )
-    colnames( sim_list_panel )[3] = "Weight"
-    
-    # calculate weights
-    
-    aggregation_all_panel = stats::aggregate( 
-      x  = as.double( sim_list_panel$Weight ),
-      by = list( as.character( sim_list_panel$CL ) ),
-      FUN = sum
-    )
-    
-    mapping_agg_stats_panel = which( aggregation_all_panel$Group.1 %in% sim_list_stats_panel[,1], arr.ind = T  )
-    sim_list_stats_panel = cbind( sim_list_stats_panel, aggregation_all_panel$x[mapping_agg_stats_panel] )
-    
-    #print("Finished aggregating, writing to database")
-    
-    Ref_Gen = rep(ref_gen, dim(sim_list_panel)[1]  )
-    sim_list_panel = cbind( sim_list_panel, Ref_Gen )
-    Ref_Gen = rep( ref_gen, dim(sim_list_stats_panel)[1]  )
-    sim_list_stats_panel = cbind( sim_list_stats_panel, Ref_Gen )
-    colnames( sim_list_stats_panel ) = c( "CL","Count","All_weights","Ref_Gen" )
-    
-    if(! exists("sim_list_global"))
-      sim_list_global <<- sim_list[0,]
-    
-    sim_list_global = rbind(sim_list_global,sim_list_panel)
-    
-    if(! exists("sim_list_stats_global"))
-      sim_list_stats_global <<- sim_list_stats_panel[0,]
-    
-    sim_list_stats_global = rbind( sim_list_stats_global, sim_list_stats_panel  )
-    
-  }
+        sim_list_panel   = sim_list[ grepl( panel, sim_list$CL) , ]
+        member_var_panel = rep( 1, dim(sim_list_panel)[1] )
+        
+        sim_list_stats_panel = aggregate( member_var_panel , by = list( sim_list_panel$CL ), FUN = sum )
+        colnames(sim_list_stats_panel) = c( "CL", "Count" )
+        
+        print("Aggregating over mutational frequency to obtain mutational weight")
+            
+        weights_panel = aggregate( member_var_panel , by = list( sim_list_panel$Fingerprint ), FUN = sum )
+        weights_panel$x = 1.0 / as.double( weights_panel$x )
+        
+        mapping_panel = match( as.character( sim_list_panel$Fingerprint ), as.character( weights_panel$Group.1) )
+        sim_list_panel = cbind( sim_list_panel, weights_panel$x[mapping_panel] )
+        colnames( sim_list_panel )[3] = "Weight"
+        
+        # calculate weights
+        
+        aggregation_all_panel = stats::aggregate( 
+          x  = as.double( sim_list_panel$Weight ),
+          by = list( as.character( sim_list_panel$CL ) ),
+          FUN = sum
+        )
+        
+        mapping_agg_stats_panel = which( aggregation_all_panel$Group.1 %in% sim_list_stats_panel[,1], arr.ind = T  )
+        sim_list_stats_panel = cbind( sim_list_stats_panel, aggregation_all_panel$x[mapping_agg_stats_panel] )
+        
+        #print("Finished aggregating, writing to database")
+        
+        Ref_Gen = rep(ref_gen, dim(sim_list_panel)[1]  )
+        sim_list_panel = cbind( sim_list_panel, Ref_Gen )
+        Ref_Gen = rep( ref_gen, dim(sim_list_stats_panel)[1]  )
+        sim_list_stats_panel = cbind( sim_list_stats_panel, Ref_Gen )
+        colnames( sim_list_stats_panel ) = c( "CL","Count","All_weights","Ref_Gen" )
+        
+        if(! exists("sim_list_global"))
+          sim_list_global <<- sim_list[0,]
+        
+        sim_list_global = rbind(sim_list_global,sim_list_panel)
+        
+        if(! exists("sim_list_stats_global"))
+          sim_list_stats_global <<- sim_list_stats_panel[0,]
+        
+        sim_list_stats_global = rbind( sim_list_stats_global, sim_list_stats_panel  )
+        
+      }
   
   print("Finished aggregating, saving to database")
   
