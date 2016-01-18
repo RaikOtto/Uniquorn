9c467548909d7170d1184c276d957176c15f6791
RaikOtto <fubioinf@gmail.com>
2016-01-18 13:52:00 +0100

    update

--- R/Create_BED_files.R
+++ R/Create_BED_files.R
@@ -3,5 +3,6 @@
 #' @param vcf_fingerprint contains the mutations that are present in the query cancer cell line's vcf file
 create_bed_file = function( sim_list, vcf_fingerprint ){
  
+    #track name="IAD93050_197_Missed" description="MissedBases_AmpliSeqID_IAD93050 solution_type=197 AmpliSeq_Version=5.2 Workflow=DNA" type=bedDetail db=hg19 color=228,26,28 priority=3
     print("test")   
 }
