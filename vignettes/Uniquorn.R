65731cabeb82da4402abcc8a7dcce239f920d99a
RaikOtto <fubioinf@gmail.com>
2016-03-07 15:29:06 +0100

    update

--- vignettes/Uniquorn.R
+++
@@ -1,5 +0,0 @@
-## ------------------------------------------------------------------------
-HT29_vcf_file = system.file("extdata/HT29.vcf.gz", package="Uniquorn")
-ident_result = identify_vcf_file( HT29_vcf_file, ref_gen = "GRCH37"  )
-head( ident_result )
-
